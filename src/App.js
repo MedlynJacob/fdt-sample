f95453345dfb62af50ac2b8ed2fa1c8e551ee31b
revand5467 <revandsunil5467@gmail.com>
2023-06-23 16:10:30 +0530

    tailwind initialized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
-import { About} from './pages';
+import styles from './style';
+import { About, Home} from './pages';
 const  App = () =>(
   <BrowserRouter>
    
       
       <Routes>
         <Route exact path='/about' element={<About />} />
+        <Route exact path='/home' element={<Home />} />
         
       </Routes>
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+        <h1 className="text-green-500">hello</h1>
+    </div>
+  )
+}
+
+export default home
